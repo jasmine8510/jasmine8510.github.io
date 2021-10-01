42f76e5b2c19b73d182360fcdbbf1796f9a90906
jasmine8510 <91745250+jasmine8510@users.noreply.github.com>
2021-10-01 11:16:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Jasmine.H"
+let userName = "jasmine8510";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
